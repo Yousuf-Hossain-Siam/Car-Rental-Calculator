570987dd2dd6e5b73b7158b0d139c5cd32bd7ad5
Yousuf Hossain Siam <yousufhossainsiam@gmail.com>
2024-10-22 22:46:19 +0600

    Initial commit

---
+++ test/car_rental_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:car_rental_calculator/car_rental_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
